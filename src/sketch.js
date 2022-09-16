f06c5f8f4d9b7320f3bf3b7f971d56b42781ab74
Vishwanath Sharma <vishwanathcodeacc@gmail.com>
2022-09-16 19:52:02 +0530

    added torch

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,15 @@
+let img;
+
+function preload() {
+  img = loadImage('../img/flashlight.svg')
+}
+
+
+
+
 function setup() {
   createCanvas(800, 600)
-
+  angleMode(DEGREES)
 
 
 }
@@ -8,8 +17,20 @@ function setup() {
 function draw() {
   background(102)
 
+  //shelf
   fill(1)
   rect(30, 30, 740, 200)
 
+  //activity area
   rect(30, 275, 740, 300)
+
+  //
+  push()
+  translate(100, 425)
+  rotate(90)
+  image(img, 0 - img.width / 2, 0 - img.width / 2)
+  imageMode(CENTER)
+  img.resize(100, 100)
+  pop()
+
 }
