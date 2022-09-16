ec115ae5198240d978b48d83a574ef0d65f6dcd7
Vishwanath Sharma <vishwanathcodeacc@gmail.com>
2022-09-16 20:18:01 +0530

    initialised objects

--- src/sketch.js
+++ src/sketch.js
@@ -5,17 +5,49 @@ function preload() {
 }
 
 
+function Mirror() {
+  this.height = 80;
+  this.width = 20;
+  this.position = { x: 250, y: 90 }
+
+  this.draw = () => {
+    fill(255, 0, 0)
+    rect(this.position.x, this.position.y, this.width, this.height)
+    // rect(width / 2, 390, 20, 80)
+  }
+}
+
+function Slab() {
+  this.height = 80;
+  this.width = 40;
+  this.position = { x: 400, y: 90 }
+
+  this.draw = () => {
+    noFill()
+    strokeWeight(5)
+    stroke(255, 0, 0)
+    rect(this.position.x, this.position.y, this.width, this.height)
+    // rect(width / 2, 390, 20, 80)
+  }
+}
+
+
+let mirror;
+let slab;
 
 
 function setup() {
   createCanvas(800, 600)
   angleMode(DEGREES)
-
+  mirror = new Mirror()
+  slab = new Slab()
 
 }
 
+
 function draw() {
   background(102)
+  noStroke()
 
   //shelf
   fill(1)
@@ -24,7 +56,12 @@ function draw() {
   //activity area
   rect(30, 275, 740, 300)
 
-  //
+  // light
+  strokeWeight(10);
+  stroke(255, 255, 255, 200)
+  line(155, 425, 390, 425)
+
+  //torch
   push()
   translate(100, 425)
   rotate(90)
@@ -32,5 +69,13 @@ function draw() {
   imageMode(CENTER)
   img.resize(100, 100)
   pop()
+
+  noStroke()
+  //dummy mirror
+  // fill(255, 0, 0)
+  // rect(width / 2, 390, 20, 80)
+
+  mirror.draw()
+  slab.draw()
 
 }
